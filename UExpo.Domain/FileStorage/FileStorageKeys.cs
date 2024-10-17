2ffe75ea6603f3a9e927600517560ffc3aaf5918
marcosxie <hsieh.marcos@gmail.com>
2024-10-17 09:20:45 -0300

    fix: Update S3 bucket name envs

--- UExpo.Domain/FileStorage/FileStorageKeys.cs
+++ UExpo.Domain/FileStorage/FileStorageKeys.cs
@@ -2,8 +2,8 @@
 
 public static class FileStorageKeys
 {
-    public static string CatalogPdf { get; } = "catalog-pdfs";
-    public static string CatalogProductsImages { get; } = "catalog-products-images";
-    public static string UserImages { get; } = "user-images";
-	public static string ChatFiles { get; } = "chat-files";
+    public static string CatalogPdf { get; } = "catalogPdfs";
+    public static string CatalogProductsImages { get; } = "catalogProductsImages";
+    public static string UserImages { get; } = "userImages";
+	public static string ChatFiles { get; } = "chatFiles";
 }
