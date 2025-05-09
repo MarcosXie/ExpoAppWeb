141170deeabb4c532068dda4079597ba7904d3d2
AndreVinni <andrevinnicius89@gmail.com>
2025-05-09 08:24:39 -0300

    feat: Adds Group entities

--- ExpoApp.Repository/Context/ExpoAppDbContext.cs
+++ ExpoApp.Repository/Context/ExpoAppDbContext.cs
@@ -4,21 +4,60 @@ using ExpoShared.Repository.Context;
 using ExpoShared.Repository.Extensions;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using ExpoShared.Repository.Configurations;
 
 namespace ExpoApp.Repository.Context;
 
-public class ExpoAppDbContext(IConfiguration configuration) : ExpoSharedContext(configuration)
+public class ExpoAppDbContext : ExpoSharedContext
 {
-	public DbSet<MomentoDao> Momentos { get; set; }
+    public ExpoAppDbContext() : this(BuildConfiguration())
+    {
+    }
 
-	protected override void OnModelCreating(ModelBuilder modelBuilder)
-	{
-		modelBuilder.AddSharedTables();
-		modelBuilder.IgnoreUExpoTables();
-		
-		// Adds ExpoApp Tables
-		modelBuilder.ApplyConfiguration(new MomentoConfiguration());
-		
-		base.OnModelCreating(modelBuilder);
-	}
+    public ExpoAppDbContext(IConfiguration configuration) : base(configuration)
+    {
+    }
+
+    private static IConfiguration BuildConfiguration()
+    {
+        // Calcula o caminho base subindo um nível e entrando na pasta ExpoApp.Api
+        var currentDirectory = Directory.GetCurrentDirectory();
+        var parentDirectory = Directory.GetParent(currentDirectory)?.FullName;
+        if (string.IsNullOrEmpty(parentDirectory))
+        {
+            throw new InvalidOperationException("Não foi possível determinar o diretório pai do diretório atual.");
+        }
+
+        var basePath = Path.Combine(parentDirectory, "ExpoApp");
+        var appsettingsPath = Path.Combine(basePath, "appsettings.json");
+
+        // Verifica se o arquivo existe
+        if (!File.Exists(appsettingsPath))
+        {
+            throw new FileNotFoundException($"O arquivo appsettings.json não foi encontrado no caminho: {appsettingsPath}");
+        }
+
+        Console.WriteLine($"Usando appsettings.json em: {appsettingsPath}");
+
+        return new ConfigurationBuilder()
+            .SetBasePath(basePath)
+            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+            .Build();
+    }
+
+    public DbSet<MomentoDao> Momentos { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.AddSharedTables();
+        modelBuilder.IgnoreUExpoTables();
+        
+        // Adds ExpoApp Tables
+        modelBuilder.ApplyConfiguration(new MomentoConfiguration());
+        modelBuilder.ApplyConfiguration(new GroupConfiguration());
+        modelBuilder.ApplyConfiguration(new GroupUserConfiguration());
+        modelBuilder.ApplyConfiguration(new GroupMessageConfiguration());
+        
+        base.OnModelCreating(modelBuilder);
+    }
 }
---
+++ ExpoApp.Repository/Migrations/20250509112016_adds-group-message-entities.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ExpoApp.Repository.Migrations
+{
+    /// <inheritdoc />
+    public partial class addsgroupmessageentities : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "group",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "char(36)", nullable: false, collation: "ascii_general_ci"),
+                    Name = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    UserDaoId = table.Column<Guid>(type: "char(36)", nullable: true, collation: "ascii_general_ci"),
+                    CreatedAt = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime(6)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("group_pkey", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_group_user_UserDaoId",
+                        column: x => x.UserDaoId,
+                        principalTable: "user",
+                        principalColumn: "Id");
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "group_message",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "char(36)", nullable: false, collation: "ascii_general_ci"),
+                    ReceiverId = table.Column<Guid>(type: "char(36)", nullable: false, collation: "ascii_general_ci"),
+                    CreatedAt = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    ChatId = table.Column<Guid>(type: "char(36)", nullable: false, collation: "ascii_general_ci"),
+                    SenderId = table.Column<Guid>(type: "char(36)", nullable: false, collation: "ascii_general_ci"),
+                    SenderName = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    SenderLang = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    SendedMessage = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    ReceiverLang = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    TranslatedMessage = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    File = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    FileName = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Deleted = table.Column<bool>(type: "tinyint(1)", nullable: false),
+                    Readed = table.Column<bool>(type: "tinyint(1)", nullable: false),
+                    ResponsedMessageId = table.Column<Guid>(type: "char(36)", nullable: true, collation: "ascii_general_ci")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("group_message_pkey", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_group_message_group_ChatId",
+                        column: x => x.ChatId,
+                        principalTable: "group",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "group_user",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "char(36)", nullable: false, collation: "ascii_general_ci"),
+                    UserId = table.Column<Guid>(type: "char(36)", nullable: false, collation: "ascii_general_ci"),
+                    GroupId = table.Column<Guid>(type: "char(36)", nullable: false, collation: "ascii_general_ci"),
+                    UserLang = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    IsAdmin = table.Column<bool>(type: "tinyint(1)", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime(6)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("group_user_pkey", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_group_user_group_UserId",
+                        column: x => x.UserId,
+                        principalTable: "group",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_group_user_user_UserId",
+                        column: x => x.UserId,
+                        principalTable: "user",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_group_UserDaoId",
+                table: "group",
+                column: "UserDaoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_group_message_ChatId",
+                table: "group_message",
+                column: "ChatId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_group_user_UserId",
+                table: "group_user",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "group_message");
+
+            migrationBuilder.DropTable(
+                name: "group_user");
+
+            migrationBuilder.DropTable(
+                name: "group");
+        }
+    }
+}
