28de8989fbe7dfb1bb1d9030b26f937ac4362ae2
AndreVinni <andrevinnicius89@gmail.com>
2025-05-12 09:19:08 -0300

    feat: Adds group structure endpoints

---
+++ ExpoApp.Repository/Migrations/20250512120501_fix-group-relationship.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ExpoApp.Repository.Migrations
+{
+    /// <inheritdoc />
+    public partial class fixgrouprelationship : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_group_user_group_UserId",
+                table: "group_user");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_group_user_GroupId",
+                table: "group_user",
+                column: "GroupId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_group_user_group_GroupId",
+                table: "group_user",
+                column: "GroupId",
+                principalTable: "group",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_group_user_group_GroupId",
+                table: "group_user");
+
+            migrationBuilder.DropIndex(
+                name: "IX_group_user_GroupId",
+                table: "group_user");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_group_user_group_UserId",
+                table: "group_user",
+                column: "UserId",
+                principalTable: "group",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
---
+++ ExpoApp/Controllers/GroupController.cs
@@ -0,0 +1,60 @@
+﻿using ExpoShared.Domain.Entities.Chats.GroupChat;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExpoApp.Api.Controllers;
+
+
+[Route("api/[controller]")]
+[ApiController]
+public class GroupController(IGroupService groupService) : ControllerBase
+{
+	[HttpPost]
+	public async Task<ActionResult> CreateGroupAsync(CreateGroupDto createGroupDto)
+	{
+		return Ok(await groupService.CreateGroupAsync(createGroupDto));
+	}
+	
+	[HttpPut("{groupId}")]
+	public async Task<ActionResult> UpdateAsync(
+		Guid groupId, string name
+	)
+	{
+		await groupService.UpdateAsync(groupId, name);
+		
+		return Ok();
+	}
+	
+	[HttpPatch("{groupId}")]
+	public async Task<ActionResult> AddMemberAsync(
+		Guid groupId, GroupAddMemberDto groupAddMemberDto
+	)
+	{
+		await groupService.AddMemberAsync(groupId, groupAddMemberDto);
+		
+		return Ok();
+	}
+	
+	[HttpPatch("{groupId}/Admin/{userId}")]
+	public async Task<ActionResult> MakeAdminAsync(Guid groupId, Guid userId)
+	{
+		await groupService.MakeAdminAsync(groupId, userId);
+		
+		return Ok();
+	}
+	
+	[HttpDelete("{groupId}/{userId}")]
+	public async Task<ActionResult> RemoveUserAsync(Guid groupId, Guid userId)
+	{
+		await groupService.RemoveUserAsync(groupId, userId);
+		
+		return Ok();
+	}
+	
+	[HttpDelete("{groupId}")]
+	public async Task<ActionResult> DeleteGroupAsync(Guid groupId)
+	{
+		await groupService.DeleteGroupAsync(groupId);
+		
+		return Ok();
+	}
+}
