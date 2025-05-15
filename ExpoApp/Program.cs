c1d2f02acdcbdca6e3395818aeb146e37b6d8533
AndreVinni <andrevinnicius89@gmail.com>
2025-05-14 21:36:57 -0300

    feat: Adds localhost as cors

--- ExpoApp/Program.cs
+++ ExpoApp/Program.cs
@@ -42,7 +42,7 @@ services.AddCors(options =>
     {
         policy
 		        // .AllowAnyOrigin()
-	          .WithOrigins("http://localhost:5174", "http://localhost:5173", "http://10.0.0.34:5173", "https://expoapp.com.br", "177.128.51.223", "172.31.0.73",  "http://177.128.51.223", "http://172.31.0.73",  "https://177.128.51.223", "https://172.31.0.73")
+	          .WithOrigins("http://localhost:5000", "http://localhost:5174", "http://localhost:5173", "http://10.0.0.34:5173", "https://expoapp.com.br", "177.128.51.223", "172.31.0.73",  "http://177.128.51.223", "http://172.31.0.73",  "https://177.128.51.223", "https://172.31.0.73")
               .AllowAnyHeader()
               .AllowAnyMethod()
 			  .WithExposedHeaders("Content-Disposition")
