1a015fab21829d1c2a7f7d8c95d81e2f673e929e
Andre <andrevinnicius89@gmail.com>
2025-05-21 09:14:16 -0300

    feat: Update

--- ExpoApp/Hubs/NotificationsHub.cs
+++ ExpoApp/Hubs/NotificationsHub.cs
@@ -1,7 +1,9 @@
 using ExpoShared.Domain.Entities.Chats.CallCenterChat;
 using ExpoShared.Domain.Entities.Chats.CartChat;
+using ExpoShared.Domain.Entities.Chats.GroupChat;
 using ExpoShared.Domain.Entities.Chats.RelationshipChat;
 using ExpoShared.Domain.Entities.Chats.Shared;
+using ExpoShared.Domain.Entities.Groups;
 using Microsoft.AspNetCore.SignalR;
 
 namespace ExpoApp.Api.Hubs;
@@ -9,6 +11,7 @@ namespace ExpoApp.Api.Hubs;
 public class NotificationsHub(
 	ICallCenterChatService callCenterService,
 	IRelationshipChatService relationshipChatService,
+	IGroupChatService groupService,
 	ICartChatService cartChatService
 ) : Hub
 {
@@ -20,6 +23,7 @@ public class NotificationsHub(
 		{
 			CallCenterNotReadedMessages = await callCenterService.GetNotReadedMessagesByUserId(userId),
 			RelationshipNotifications = await relationshipChatService.GetNotReadedMessagesAsync(userId),
+			GroupNotifications = await groupService.GetNotReadedMessagesAsync(userId),
 			CartNotifications = await cartChatService.GetNotReadedMessagesAsync(userId),
 			NewCarts = await cartChatService.GetNewCartsAsync(userId),
 		};
