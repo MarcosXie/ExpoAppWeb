9f9cbf342b659a93344b35a5050cf40c2d3d3e24
Andre <andrevinnicius89@gmail.com>
2025-06-09 07:13:28 -0300

    feat: Adds is call center flag in relationship

--- ExpoApp.Repository/Migrations/20250609101139_adds-call-center-flag-into-user.Designer.cs
+++ ExpoApp.Repository/Migrations/20250609101139_adds-call-center-flag-into-user.Designer.cs
@@ -4,6 +4,7 @@ using ExpoApp.Repository.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ExpoApp.Repository.Migrations
 {
     [DbContext(typeof(ExpoAppDbContext))]
-    partial class ExpoAppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250609101139_adds-call-center-flag-into-user")]
+    partial class addscallcenterflagintouser
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
