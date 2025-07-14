6fa62ed6d0cca535f80348eb7d1d389de13c36b6
Andre <andrevinnicius89@gmail.com>
2025-07-14 09:53:04 -0300

    feat: adds force original language message

---
+++ ExpoApp.Repository/Migrations/20250714125047_adds-force-original-language-flag.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ExpoApp.Repository.Migrations
+{
+    /// <inheritdoc />
+    public partial class addsforceoriginallanguageflag : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "ForceOriginalLanguage",
+                table: "relationship_message",
+                type: "tinyint(1)",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "ForceOriginalLanguage",
+                table: "group_message",
+                type: "tinyint(1)",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "ForceOriginalLanguage",
+                table: "cart_message",
+                type: "tinyint(1)",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "ForceOriginalLanguage",
+                table: "call_center_message",
+                type: "tinyint(1)",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ForceOriginalLanguage",
+                table: "relationship_message");
+
+            migrationBuilder.DropColumn(
+                name: "ForceOriginalLanguage",
+                table: "group_message");
+
+            migrationBuilder.DropColumn(
+                name: "ForceOriginalLanguage",
+                table: "cart_message");
+
+            migrationBuilder.DropColumn(
+                name: "ForceOriginalLanguage",
+                table: "call_center_message");
+        }
+    }
+}
