58e3319092a848bb97399e2eac52301fd060e882
Andre <andrevinnicius89@gmail.com>
2025-07-17 09:46:15 -0300

    feat: adds loro

---
+++ ExpoApp/Controllers/SpeechController.cs
@@ -0,0 +1,34 @@
+﻿using ExpoShared.Domain.SpeechTranslation;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExpoApp.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class SpeechController : ControllerBase
+{
+	private readonly ISpeechService _speechService;
+
+	public SpeechController(ISpeechService speechService)
+	{
+		_speechService = speechService;
+	}
+
+	[HttpPost]
+	public async Task<IActionResult> Translate([FromForm] TranslationRequestDto request)
+	{
+		if (request.AudioFile.Length == 0)
+		{
+			return BadRequest("Audio file is required.");
+		}
+
+		var result = await _speechService.TranslateAudioAsync(request);
+
+		if (result == null)
+		{
+			return StatusCode(500, "Failed to process audio translation.");
+		}
+
+		return Ok(result);
+	}
+}
