3b8eecffbd8943c80da9ce4d5025fae24178835f
Andre <andrevinnicius89@gmail.com>
2025-08-11 10:07:41 -0300

    feat: Update

--- ExpoApp.Domain/EmailTemplates/ConfirmEmailTemplate.cs
+++ ExpoApp.Domain/EmailTemplates/ConfirmEmailTemplate.cs
@@ -21,7 +21,7 @@ public class ConfirmationEmailTemplate : IConfirmationEmailTemplate
        <table role=""presentation"" border=""0"" cellpadding=""0"" cellspacing=""0"">
          <tr>
            <td style=""vertical-align: middle;"">
-             <a href=""{0}/025_verify_email/{1}/{2}/{3}""
+             <a href=""{0}/{4}/{1}/{2}/{3}""
                 style=""background-color: #4CAF50; color: white; padding: 10px 20px; text-decoration: none; border-radius: 4px;"">
                Validate
              </a>
