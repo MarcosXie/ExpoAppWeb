ad977c962537eb1e6ce9b46c31926d41a40e2dc7
Andre <andrevinnicius89@gmail.com>
2025-08-20 16:17:59 -0300

    feat: update

---
+++ ExpoApp.Repository/Migrations/20250820191142_adds-expired-date-into-code.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ExpoApp.Repository.Migrations
+{
+    /// <inheritdoc />
+    public partial class addsexpireddateintocode : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ValidationCodeExpireDate",
+                table: "user_loro",
+                type: "datetime(6)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ValidationCodeExpireDate",
+                table: "user_loro");
+        }
+    }
+}
